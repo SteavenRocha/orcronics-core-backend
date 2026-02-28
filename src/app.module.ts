c1a9d0fa1888c454d31d6ec117619985c9d5f505
SteavenRocha <steavenrocha@gmail.com>
2026-02-27 19:20:33 -0500

    database connection configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,35 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true // No usar en producción - Cambiar a false
+      }),
+
+      inject: [ConfigService],
+    }),
+
+    CustomersModule,
+
+  ],
   controllers: [],
   providers: [],
 })
