db98e424e782cee0368a5393d9ed0164f8d365f8
SteavenRocha <steavenrocha@gmail.com>
2026-02-28 02:37:44 -0500

    add create-branch.dto and update-branch-dto

--- src/branches/dto/create-branch.dto.ts
+++ src/branches/dto/create-branch.dto.ts
@@ -1,1 +1,30 @@
-export class CreateBranchDto {}
+import { IsNotEmpty, IsOptional, IsString, IsUUID, MaxLength, MinLength } from "class-validator";
+
+export class CreateBranchDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(1)
+    @MaxLength(255)
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    address?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(255)
+    contact_name?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(9)
+    @MaxLength(20)
+    contact_phone?: string;
+
+    @IsUUID()
+    @IsNotEmpty()
+    customer_id: string;
+}
--- src/branches/dto/update-branch.dto.ts
+++ src/branches/dto/update-branch.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateBranchDto } from './create-branch.dto';
 
-export class UpdateBranchDto extends PartialType(CreateBranchDto) {}
+export class UpdateBranchDto extends PartialType(OmitType(CreateBranchDto, ['customer_id'] as const)) { }
