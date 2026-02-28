9740c28784b28020da2c2d717dd5f43f27ca60cd
SteavenRocha <steavenrocha@gmail.com>
2026-02-27 23:38:59 -0500

    add create-customers.dto

--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,1 +1,15 @@
-export class CreateCustomerDto {}
+import { IsString, IsUrl, IsNotEmpty, MaxLength, IsOptional, MinLength } from "class-validator";
+
+export class CreateCustomerDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(1)
+    @MaxLength(255)
+    name: string;
+
+    @IsUrl()
+    @IsOptional()
+    logo_url?: string;
+
+}
