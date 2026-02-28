bdc60c589a73ea76306887bb02da73de701bd5f9
SteavenRocha <steavenrocha@gmail.com>
2026-02-27 19:57:42 -0500

    add customers repository

--- src/customers/entities/customer.entity.ts
+++ src/customers/entities/customer.entity.ts
@@ -1,1 +1,27 @@
-export class Customer {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('customers')
+export class Customer {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    name: string;
+
+    @Column({ type: 'text', nullable: true })
+    logo_url: string;
+
+    @Column({ type: 'boolean', default: true })
+    is_active: boolean;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    created_at: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    updated_at: Date;
+
+    @DeleteDateColumn({ type: 'timestamp' })
+    deleted_at: Date;
+
+}
